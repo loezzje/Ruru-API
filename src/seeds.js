76964dcd4467d192d69c8677db4e2e625c9ce6e5
almozaz <alacruzmozaz@gmail.com>
2017-10-16 17:26:54 +0200

    .

--- src/seeds.js
+++ src/seeds.js
@@ -24,83 +24,83 @@ const categories = [
     frontPage: true}
 ];
 
-// const feathersClient = feathers();
-//
-// feathersClient
-//   .configure(hooks())
-//   .configure(rest('http://localhost:3030').superagent(superagent));
-//
+const feathersClient = feathers();
 
-// categories.map((categories) => {
-//   feathersClient.service('categories').create(categories)
-//     .then((result) => {
-//       console.log('Category seeded...', result.name);
-//     }).catch((error) => {
-//       console.error('Error seeding categories!', error.message);
-//     });
-// });
+feathersClient
+  .configure(hooks())
+  .configure(rest('http://localhost:3030').superagent(superagent));
+
+
+categories.map((categories) => {
+  feathersClient.service('categories').create(categories)
+    .then((result) => {
+      console.log('Category seeded...', result.name);
+    }).catch((error) => {
+      console.error('Error seeding categories!', error.message);
+    });
+});
 
 
 // comment the section above, between the previous comment and this comment
 // uncomment the entire section below, and seed again: yarn run seed.
 
-const organizations = [{
-  name: 'COA - Centre of Asylum',
-  logo: 'https://www.coa.nl/sites/www.coa.nl/themes/coa_bs/logo.png',
-  tagline: 'Questions about your asylum process? Contact COA',
-  about: 'COA is responsible for the reception, supervision and departure (from the reception location) of asylum seekers coming to the Netherlands',
-  features: ['Payment of pocket money', 'Referral of specialist services', 'Counseling and support', 'Accommodation advise'],
-  website: 'www.coa.nl',
-  phone: '0887157000',
-  address: 'Rijsntraat 8 2515 XP Den Haag',
-  categories: ['59e4c89b1f1bf8434bba1927'],
-  frontpage: true
-}, {
-  name: 'IND - Immigration and Naturalization',
-  logo: 'https://ind.nl/_catalogs/masterpage/ind/img/logo.png',
-  about: 'IND assess all applications from foriegn nationals who want to live in the Netherlands or want to become Dutch citizens. They also handle your asylum procedure.',
-  features: ['Asylum procedure'],
-  website: 'www.ind.nl',
-  phone: '0880430430',
-  address: 'Stadhouderskade 85 1073 AT Amsterdam',
-  categories: ['59e4c89b1f1bf8434bba1928'],
-  frontpage: false
-},
-{
-  name: 'I Amsterdam',
-  logo: 'http://allemediavacatures.nl/wp-content/uploads/2017/02/I-amsterdam-logo.png',
-  tagline: 'All about visiting, living and working in Amsterdam',
-  about: 'You can find practical information about where to look for work, accommodation or studying in Amsterdam. It is meant for tourists, but also acts as the English information site of City Counsil',
-  features: ['Finding work', 'Practical information about living in Amsterdam, inclusing taxes', 'Information about Universities in Amsterdam'],
-  website: 'www.iamsterdam.com/en',
-  categories: ['59e4c89b1f1bf8434bba1927', '59e4c89b1f1bf8434bba1929', '59e4c89b1f1bf8434bba192a'],
-  frontpage: false
-},
-{
-  name: 'Vluchtelingenwerk',
-  logo: 'https://www.vluchtelingenwerk.nl/sites/public/u895/Logo_VluchtelingenWerk.jpg',
-  about: 'Vluchtelingenwerk can support you during your asylum procedure.',
-  features: ['Asylum procedure', 'Family reunion', 'Support with return requests'],
-  website: 'www.vluchtelingenwerk.nl',
-  phone: '0203467200',
-  address: 'Surinameplein 122 1058 GV Amsterdam',
-  categories: ['59e4c89b1f1bf8434bba1929', '59e4c89b1f1bf8434bba192b'],
-  frontpage: true
-}
-];
-
-// const feathersClient = feathers();
+// const organizations = [{
+//   name: 'COA - Centre of Asylum',
+//   logo: 'https://www.coa.nl/sites/www.coa.nl/themes/coa_bs/logo.png',
+//   tagline: 'Questions about your asylum process? Contact COA',
+//   about: 'COA is responsible for the reception, supervision and departure (from the reception location) of asylum seekers coming to the Netherlands',
+//   features: ['Payment of pocket money', 'Referral of specialist services', 'Counseling and support', 'Accommodation advise'],
+//   website: 'www.coa.nl',
+//   phone: '0887157000',
+//   address: 'Rijsntraat 8 2515 XP Den Haag',
+//   categories: ['59e4c89b1f1bf8434bba1927'],
+//   frontpage: true
+// }, {
+//   name: 'IND - Immigration and Naturalization',
+//   logo: 'https://ind.nl/_catalogs/masterpage/ind/img/logo.png',
+//   about: 'IND assess all applications from foriegn nationals who want to live in the Netherlands or want to become Dutch citizens. They also handle your asylum procedure.',
+//   features: ['Asylum procedure'],
+//   website: 'www.ind.nl',
+//   phone: '0880430430',
+//   address: 'Stadhouderskade 85 1073 AT Amsterdam',
+//   categories: ['59e4c89b1f1bf8434bba1928'],
+//   frontpage: false
+// },
+// {
+//   name: 'I Amsterdam',
+//   logo: 'http://allemediavacatures.nl/wp-content/uploads/2017/02/I-amsterdam-logo.png',
+//   tagline: 'All about visiting, living and working in Amsterdam',
+//   about: 'You can find practical information about where to look for work, accommodation or studying in Amsterdam. It is meant for tourists, but also acts as the English information site of City Counsil',
+//   features: ['Finding work', 'Practical information about living in Amsterdam, inclusing taxes', 'Information about Universities in Amsterdam'],
+//   website: 'www.iamsterdam.com/en',
+//   categories: ['59e4c89b1f1bf8434bba1927', '59e4c89b1f1bf8434bba1929', '59e4c89b1f1bf8434bba192a'],
+//   frontpage: false
+// },
+// {
+//   name: 'Vluchtelingenwerk',
+//   logo: 'https://www.vluchtelingenwerk.nl/sites/public/u895/Logo_VluchtelingenWerk.jpg',
+//   about: 'Vluchtelingenwerk can support you during your asylum procedure.',
+//   features: ['Asylum procedure', 'Family reunion', 'Support with return requests'],
+//   website: 'www.vluchtelingenwerk.nl',
+//   phone: '0203467200',
+//   address: 'Surinameplein 122 1058 GV Amsterdam',
+//   categories: ['59e4c89b1f1bf8434bba1929', '59e4c89b1f1bf8434bba192b'],
+//   frontpage: true
+// }
+// ];
 //
-// feathersClient
-//   .configure(hooks())
-//   .configure(rest('http://localhost:3030').superagent(superagent));
-//
-//
-// organizations.map((organizations) => {
-//   feathersClient.service('organizations').create(organizations)
-//     .then((result) => {
-//       console.log('Organization seeded...', result.name);
-//     }).catch((error) => {
-//       console.error('Error seeding organizations!', error.message);
-//     });
-// });
+// // const feathersClient = feathers();
+// //
+// // feathersClient
+// //   .configure(hooks())
+// //   .configure(rest('http://localhost:3030').superagent(superagent));
+// //
+// //
+// // organizations.map((organizations) => {
+// //   feathersClient.service('organizations').create(organizations)
+// //     .then((result) => {
+// //       console.log('Organization seeded...', result.name);
+// //     }).catch((error) => {
+// //       console.error('Error seeding organizations!', error.message);
+// //     });
+// // });
