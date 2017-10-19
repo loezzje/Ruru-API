02ea2275d281a5da0870eaa4a35b719f10178891
almozaz <alacruzmozaz@gmail.com>
2017-10-19 13:58:53 +0200

    Adding organization to categories qhen creating now works

--- src/hooks/add-to-category.js
+++ src/hooks/add-to-category.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function addToCategory (hook) {
-    debugger;
     hook.data._id = new mongoose.Types.ObjectId;
+    debugger;
     hook.data.categories.map(function(categoryId){
-      hook.app.service('organizations')
-        .findByIdAndUpdate(categoryId, { $addToSet: { organizationsId: hook.data._id }});
+      hook.app.service('categories')
+        .patch({ _id: categoryId }, { $addToSet: { organizationsId: hook.data._id }});
     });
-
+    debugger;
     return Promise.resolve(hook);
   };
 };
