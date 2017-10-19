f052201fb05631c6ca0f698340aeed2360a0c6e8
almozaz <alacruzmozaz@gmail.com>
2017-10-19 11:49:38 +0200

    add-to-category adds the new organization to categories's orgs. Debugging pending

--- src/hooks/add-to-category.js
+++ src/hooks/add-to-category.js
@@ -1,10 +1,14 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+var mongoose = require('mongoose');
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function addToCategory (hook) {
-    // Hooks can either return nothing or a promise
-    // that resolves with the `hook` object for asynchronous operations
+    debugger;
+    hook.data._id = new mongoose.Types.ObjectId;
+    hook.data.categories.map(function(categoryId){
+      hook.app.service('organizations')
+        .findByIdAndUpdate(categoryId, { $addToSet: { organizationsId: hook.data._id }});
+    });
+
     return Promise.resolve(hook);
   };
 };
